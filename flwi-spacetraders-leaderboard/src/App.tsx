db5324b3b41b2c2ad4ea4f8958a938d88ccaad6c
Florian Witteler <github@flwi.de>
2024-04-05 14:27:54 +0200

    use tailwindcss

--- flwi-spacetraders-leaderboard/src/App.tsx
+++ flwi-spacetraders-leaderboard/src/App.tsx
@@ -1,26 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useEffect, useState} from 'react'
+import {ApiResetDate, CrateService} from "../generated-src";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [resetDates, setResetDates] = useState(Array.of<ApiResetDate>())
+
+  useEffect(() => {
+    // Execute the fetch call on component mount
+    CrateService.getResetDates()
+      .then((fetchedData) => {
+        // Set the fetched data to state
+        setResetDates(fetchedData.reset_dates);
+      })
+      .catch((error) => {
+        console.error('Error fetching data: ', error);
+        // Optionally, handle errors or set error state here
+      });
+  }, []); // Empty dependency array means this effect runs once on mount
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <h1>Flwi Spacetraders Leaderboard</h1>
+      <div className="bg-amber-400">
+        <ul>
+          {resetDates.map((item, index) => <li key={index}>{item}</li>)}
+
+        </ul>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
