d2abf1406e2baeb3a42c9b766db0a7b868ad90c4
Florian Witteler <github@flwi.de>
2024-04-06 17:33:48 +0200

    repolaced react-router by tanstack router

--- flwi-spacetraders-leaderboard/src/main.tsx
+++ flwi-spacetraders-leaderboard/src/main.tsx
@@ -1,30 +1,29 @@
-import React from 'react'
+import {StrictMode} from 'react'
 import ReactDOM from 'react-dom/client'
+import {createRouter, RouterProvider} from '@tanstack/react-router'
 
-import {createBrowserRouter, RouterProvider,} from "react-router-dom";
 import './index.css'
-import Root, {fetchData} from "./routes/root";
-import ErrorPage from "./error-page.tsx";
-import ResetPage, {loader} from "./routes/reset-page.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    loader: fetchData,
-    children: [
-      {
-        path: "reset/:resetDate",
-        element: <ResetPage/>,
-        loader: loader //this is fine...
-      },
-    ],
-  },
-]);
+// Import the generated route tree
+import {routeTree} from './routeTree.gen'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+// Create a new router instance
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+// Render the app
+const rootElement = document.getElementById('root')!
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  )
+}
