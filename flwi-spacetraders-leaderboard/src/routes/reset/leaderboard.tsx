bbcff3d54b41591fcba03f8589c550ea16e062f4
Florian Witteler <github@flwi.de>
2024-04-08 23:11:01 +0200

    integrated @tanstack/react-table

--- flwi-spacetraders-leaderboard/src/routes/reset/leaderboard.tsx
+++ flwi-spacetraders-leaderboard/src/routes/reset/leaderboard.tsx
@@ -1,11 +1,31 @@
 import {createFileRoute} from '@tanstack/react-router'
-import {CrateService} from "../../../generated";
+import {ApiLeaderboardEntry, CrateService} from "../../../generated";
+
+import {createColumnHelper, flexRender, getCoreRowModel, getSortedRowModel, SortingState, useReactTable,} from '@tanstack/react-table'
+import React from "react";
 
 
 type LeaderboardSearch = {
   resetDate?: string
 }
 
+const columnHelper = createColumnHelper<ApiLeaderboardEntry>()
+let numberFmt = new Intl.NumberFormat();
+numberFmt.format(1234567)
+const columns = [
+  columnHelper.accessor('agentSymbol', {
+    cell: info => info.getValue(),
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor('credits', {
+    cell: info => numberFmt.format(info.getValue()),
+    footer: info => info.column.id,
+  }),
+  columnHelper.accessor('shipCount', {
+    cell: info => numberFmt.format(info.getValue()),
+    footer: info => info.column.id,
+  }),
+]
 
 export const Route = createFileRoute('/reset/leaderboard')({
   component: LeaderboardComponent,
@@ -31,12 +51,88 @@ export const Route = createFileRoute('/reset/leaderboard')({
 
 function LeaderboardComponent() {
   const {resetDateToUse, leaderboard} = Route.useLoaderData()
+  const [sorting, setSorting] = React.useState<SortingState>([])
 
+  const table = useReactTable({
+    data: leaderboard.leaderboardEntries,
+    columns,
+    state: {sorting},
+    onSortingChange: setSorting,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    debugTable: true,
+  })
   return (
     <>
       <div className="flex flex-col">
         <h1>Leaderboard for reset {resetDateToUse}</h1>
-        <pre>{JSON.stringify(leaderboard, null, 2)}</pre>
+        <div className="p-2">
+          <div className="h-2"/>
+          <table>
+            <thead>
+            {table.getHeaderGroups().map(headerGroup => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map(header => {
+                  return (
+                    <th key={header.id} colSpan={header.colSpan}>
+                      {header.isPlaceholder ? null : (
+                        <div
+                          className={
+                            header.column.getCanSort()
+                              ? 'cursor-pointer select-none'
+                              : ''
+                          }
+                          onClick={header.column.getToggleSortingHandler()}
+                          title={
+                            header.column.getCanSort()
+                              ? header.column.getNextSortingOrder() === 'asc'
+                                ? 'Sort ascending'
+                                : header.column.getNextSortingOrder() === 'desc'
+                                  ? 'Sort descending'
+                                  : 'Clear sort'
+                              : undefined
+                          }
+                        >
+                          {flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                          {{
+                            asc: ' 🔼',
+                            desc: ' 🔽',
+                          }[header.column.getIsSorted() as string] ?? null}
+                        </div>
+                      )}
+                    </th>
+                  )
+                })}
+              </tr>
+            ))}
+            </thead>
+            <tbody>
+            {table
+              .getRowModel()
+              .rows
+              .map(row => {
+                return (
+                  <tr key={row.id}>
+                    {row.getVisibleCells().map(cell => {
+                      return (
+                        <td key={cell.id}>
+                          {flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext()
+                          )}
+                        </td>
+                      )
+                    })}
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+          <div>{table.getRowModel().rows.length.toLocaleString()} Rows</div>
+        </div>
       </div>
     </>
   )
