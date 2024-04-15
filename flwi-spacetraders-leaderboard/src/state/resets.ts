a8d2572c45a82ef487fd1d7491c4964cc6daaf61
Florian Witteler <github@flwi.de>
2024-04-15 19:28:24 +0200

    Revert "further playing with atoms"
    This reverts commit 01efcd0c04eaf868c8d0e9d321a9522b80f6b89c.

--- flwi-spacetraders-leaderboard/src/state/resets.ts
+++ flwi-spacetraders-leaderboard/src/state/resets.ts
@@ -1,5 +1,4 @@
 import {atom, createStore} from 'jotai'
-import {GetLeaderboardForResetResponseContent} from "../../generated";
 
 export const store = createStore();
 
@@ -7,9 +6,7 @@ export const store = createStore();
 export const resetDatesAtom = atom([] as string[])
 
 
-export interface ResetData {
-  resetDate: string,
-  loadedAgents: string[]
-}
-export const resetDataAtom = atom(new Map<string, GetLeaderboardForResetResponseContent>)
+interface ResetData {
 
+}
+export const resetDataAtom = atom(new Map<string, ResetData>)
