29dab1d74af264b99c22757579da60569ff2d5e7
Florian Witteler <github@flwi.de>
2024-04-16 14:15:31 +0200

    zustand to the rescue :tada:

--- flwi-spacetraders-leaderboard/src/lib/utils.ts
+++ flwi-spacetraders-leaderboard/src/lib/utils.ts
@@ -1,8 +1,10 @@
-import {type ClassValue, clsx} from "clsx"
-import {twMerge} from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { ApiLeaderboardEntry, CrateService } from "../../generated";
+import { create } from "zustand";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export function zip<T, U>(a: T[], b: U[]): [T, U][] {
@@ -13,3 +15,78 @@ export function zip<T, U>(a: T[], b: U[]): [T, U][] {
   }
   return result;
 }
+
+interface ApiAgentHistory {
+  agentSymbol: string;
+  entries: string[];
+}
+
+interface ResetFetchState {
+  lastRefresh: Date;
+  leaderboard: ApiLeaderboardEntry[];
+  historyData: ApiAgentHistory[];
+}
+
+interface FetchState {
+  fetchStates: Map<string, ResetFetchState>;
+}
+
+type Action = {
+  updateFetchData: (
+    resetDate: string,
+    newAgentSelection: string[],
+  ) => Promise<void>;
+};
+
+async function loadResetFetchState(
+  resetDate: string,
+): Promise<ResetFetchState> {
+  let leaderboard = await CrateService.getLeaderboard({ resetDate });
+  return {
+    historyData: [],
+    lastRefresh: new Date(),
+    leaderboard: leaderboard.leaderboardEntries,
+  };
+}
+
+async function computeNewState(
+  resetDate: string,
+  state: FetchState,
+  newAgentSelection: string[],
+): Promise<FetchState> {
+  let current =
+    state.fetchStates.get(resetDate) ?? (await loadResetFetchState(resetDate));
+
+  let currentAgentSymbols = current.historyData.map((e) => e.agentSymbol);
+
+  let notIncluded = newAgentSelection.filter(
+    (queryParamAgent) => !currentAgentSymbols.includes(queryParamAgent),
+  );
+
+  console.log("currentAgentSymbols in cache", currentAgentSymbols);
+  console.log("newAgentSelection", newAgentSelection);
+  console.log("notIncluded", notIncluded);
+
+  let newEntries = notIncluded.map((agentSymbol) => ({
+    agentSymbol,
+    entries: [],
+  }));
+
+  let updated = {
+    fetchStates: new Map(state.fetchStates).set(resetDate, {
+      ...current,
+      historyData: newEntries,
+    }),
+  };
+
+  return updated;
+}
+
+export const useFetchState = create<FetchState & Action>((set, get) => ({
+  fetchStates: new Map(),
+  updateFetchData: async (resetDate: string, newAgentSelection: string[]) => {
+    let current = get();
+    let newState = await computeNewState(resetDate, current, newAgentSelection);
+    set(() => newState);
+  },
+}));
