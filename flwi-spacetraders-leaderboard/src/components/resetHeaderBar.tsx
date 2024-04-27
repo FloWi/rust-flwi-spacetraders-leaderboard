f5fd64c6474e89f33cc546bf4f04c46ea8874022
Florian Witteler <github@flwi.de>
2024-04-27 15:07:59 +0200

    created own MyLink component to properly wrap the NavigationMenuLink
    necessary to capture the "active" prop

--- flwi-spacetraders-leaderboard/src/components/resetHeaderBar.tsx
+++ flwi-spacetraders-leaderboard/src/components/resetHeaderBar.tsx
@@ -1,5 +1,5 @@
-import { Link } from "@tanstack/react-router";
-import { Separator } from "../@/components/ui/separator.tsx";
+import {Link, LinkProps, useMatchRoute} from "@tanstack/react-router";
+import {Separator} from "../@/components/ui/separator.tsx";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -7,6 +7,7 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
 } from "../@/components/ui/navigation-menu.tsx";
 
 interface ResetHeaderBarProps {
@@ -14,10 +15,26 @@ interface ResetHeaderBarProps {
   selectedAgents?: string[];
 }
 
+function MyLink(props: LinkProps & { className?: string; content: string }) {
+  const matchRoute = useMatchRoute();
+
+  let isMatch = !!matchRoute({to: props.to});
+  return (
+    <NavigationMenuLink
+      asChild
+      active={isMatch}
+      className={navigationMenuTriggerStyle()}
+    >
+      <Link {...props}>{props.content}</Link>
+    </NavigationMenuLink>
+  );
+}
+
 export const ResetHeaderBar = ({
-  resetDate,
-  selectedAgents,
-}: ResetHeaderBarProps) => {
+                                 resetDate,
+                                 selectedAgents,
+                               }: ResetHeaderBarProps) => {
+
   return (
     <div className="min-w-full flex flex-col gap-4">
       <div className="flex flex-row items-center h-5 space-x-2">
@@ -32,51 +49,29 @@ export const ResetHeaderBar = ({
               <NavigationMenuContent>
                 <ul className="flex flex-col w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
                   <li>
-                    <NavigationMenuLink asChild>
-                      <Link
-                        to="/resets/$resetDate/leaderboard"
-                        params={{ resetDate }}
-                        search={{ agents: selectedAgents }}
-                        className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground
-              [&.active]:bg-muted
-              [&.active]:font-medium
-              [&.active]:text-primary
-"
-                      >
-                        Leaderboard
-                      </Link>
-                    </NavigationMenuLink>
+                    <MyLink
+                      to="/resets/$resetDate/leaderboard"
+                      params={{resetDate}}
+                      search={{agents: selectedAgents}}
+                      content="Leaderboard"
+                    ></MyLink>
                   </li>
                   <li>
-                    <NavigationMenuLink asChild>
-                      <Link
-                        to="/resets/$resetDate/history"
-                        params={{ resetDate }}
-                        search={{ selectedAgents: selectedAgents }}
-                        className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground
-              [&.active]:bg-muted
-              [&.active]:font-medium
-              [&.active]:text-primary
-"
-                      >
-                        History
-                      </Link>
-                    </NavigationMenuLink>
+                    <MyLink
+                      to="/resets/$resetDate/history"
+                      params={{resetDate: resetDate}}
+                      search={{selectedAgents: selectedAgents}}
+                      className={navigationMenuTriggerStyle()}
+                      content="History"
+                    />
                   </li>
                   <li>
-                    <NavigationMenuLink asChild>
-                      <Link
-                        to="/resets/$resetDate/jump-gate"
-                        params={{ resetDate }}
-                        className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground
-              [&.active]:bg-muted
-              [&.active]:font-medium
-              [&.active]:text-primary
-"
-                      >
-                        Jump-Gate Overview
-                      </Link>
-                    </NavigationMenuLink>
+                    <MyLink
+                      to="/resets/$resetDate/jump-gate"
+                      params={{resetDate}}
+                      className={navigationMenuTriggerStyle()}
+                      content="Jump-Gate Overview"
+                    ></MyLink>
                   </li>
                 </ul>
               </NavigationMenuContent>
@@ -84,7 +79,7 @@ export const ResetHeaderBar = ({
           </NavigationMenuList>
         </NavigationMenu>
       </div>
-      <Separator />
+      <Separator/>
     </div>
   );
 };
