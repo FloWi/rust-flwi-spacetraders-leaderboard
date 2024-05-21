31c38235aac66216c9ff17890cb8d9bd20aef9da
Florian Witteler <github@flwi.de>
2024-05-21 14:23:21 +0200

    implemented "materialized view" table refresh; background collector uses separate connection pool

--- src/db.rs
+++ src/db.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use chrono::{Local, NaiveDate, NaiveDateTime};
 use serde::{Deserialize, Serialize};
 use sqlx::sqlite::SqlitePoolOptions;
-use sqlx::{Error, Pool, Sqlite};
+use sqlx::{Error, Pool, Sqlite, Transaction};
 
 use crate::leaderboard_model::{
     LeaderboardCurrentAgentInfo, LeaderboardCurrentConstructionInfo, LeaderboardStaticAgentInfo,
@@ -630,6 +630,95 @@ select id, reset_id, trade_symbol, required
         .await?;
     }
 
+    Ok(())
+}
+
+#[tracing::instrument(level = "debug")]
+pub(crate) async fn refresh_fake_materialized_view(pool: &Pool<Sqlite>) -> anyhow::Result<()> {
+    let tx: Transaction<'static, Sqlite> = pool.begin().await?;
+
+    sqlx::query!(
+        r#"
+    delete
+    from mat_view_material_delivery_events
+    where true
+        "#
+    )
+    .execute(pool)
+    .await?;
+
+    sqlx::query!(
+        r#"
+    delete
+    from mat_view_current_construction_progress
+    where true
+        "#
+    )
+    .execute(pool)
+    .await?;
+
+    sqlx::query!(
+        r#"
+    insert into mat_view_material_delivery_events (reset_id,
+                                                   construction_site_id,
+                                                   construction_requirement_id,
+                                                   first_ts,
+                                                   query_time,
+                                                   duration_seconds,
+                                                   delivery_event)
+    select reset_id,
+           construction_site_id,
+           construction_requirement_id,
+           first_ts,
+           query_time,
+           duration_seconds,
+           delivery_event
+    from v_material_delivery_events
+        "#
+    )
+    .execute(pool)
+    .await?;
+
+    sqlx::query!(
+        r#"
+    insert into mat_view_current_construction_progress (is_jump_gate_complete,
+                                                        construction_site_id,
+                                                        construction_requirement_id,
+                                                        fulfilled,
+                                                        reset_id)
+    with last_entry_of_reset as (select *
+                                 from (select r.reset_id
+                                            , r.first_ts
+                                            , r.reset
+                                            , row_number() over (partition by jr.reset_id order by jr.query_time desc) as rn
+                                            , jr.query_time                                                            as ts_latest_entry_of_reset
+                                            , jr.id                                                                    as job_run_id_latest_entry
+                                       from reset_date r
+                                                join main.job_run jr
+                                                     on r.reset_id = jr.reset_id) sub
+                                 where rn = 1)
+
+    select cl.is_complete as is_jump_gate_complete
+         , cl.construction_site_id
+         , cml.construction_requirement_id
+         , cml.fulfilled
+         , l.reset_id
+    from construction_material_log cml
+             join main.construction_log cl
+                  on cml.construction_log_id = cl.id
+             join main.construction_site cs
+                  on cl.construction_site_id = cs.id
+             join main.construction_requirement cr
+                  on cml.construction_requirement_id = cr.id
+             join last_entry_of_reset l
+                  on l.job_run_id_latest_entry = cl.job_id
+    where required > 1
+            "#
+    )
+    .execute(pool)
+    .await?;
+
+    let _ = tx.commit().await?;
     Ok(())
 }
 
