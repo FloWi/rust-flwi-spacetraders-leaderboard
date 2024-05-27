6bef742cb3eeffa40f719302913b2d13c603d6a8
Florian Witteler <github@flwi.de>
2024-05-28 00:30:43 +0200

    use nice table in reset-index page

--- flwi-spacetraders-leaderboard/src/routes/resets/index.tsx
+++ flwi-spacetraders-leaderboard/src/routes/resets/index.tsx
@@ -2,6 +2,18 @@ import { createFileRoute, Link, Outlet } from "@tanstack/react-router";
 
 import { OpenAPI } from "../../../generated";
 import { resetDatesQueryOptions } from "../../utils/queryOptions.ts";
+import { prettyDuration } from "../../lib/formatters.ts";
+import {
+  Table,
+  TableCaption,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+} from "../../@/components/ui/table.tsx";
+import { CircleCheckBigIcon } from "lucide-react";
+import React from "react";
 
 OpenAPI.BASE = "http://localhost:8080";
 
@@ -20,29 +32,42 @@ function ResetRouteComponent() {
 
   return (
     <>
-      <div className="sub-header">
-        <h2>Resets</h2>
+      <div className="sub-header flex flex-row gap-2">
+        <h2 className="text-2xl font-bold">Resets</h2>
       </div>
       <div className="content flex flex-row gap-6">
         <div>
           <nav>
-            <ul>
-              {resetDates
-                .toSorted()
-                .toReversed()
-                .map((date, idx) => (
-                  <li key={idx}>
-                    <Link
-                      to="/resets/$resetDate/leaderboard"
-                      params={{ resetDate: date.resetDate }}
-                      // search={{agents: ["WHYANDO", "SG-1-DEVX56"]}}
-                      className="[&.active]:font-bold"
-                    >
-                      {date.resetDate}
-                    </Link>
-                  </li>
-                ))}
-            </ul>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[100px]">Reset Date</TableHead>
+                  <TableHead className="text-right">Duration</TableHead>
+                  <TableHead>Is Ongoing</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {resetDates
+                  .toSorted()
+                  .toReversed()
+                  .map((date, idx) => (
+                    <TableRow key={idx}>
+                      <TableCell className="font-medium">
+                        <Link
+                          to="/resets/$resetDate/leaderboard"
+                          params={{ resetDate: date.resetDate }}
+                          // search={{agents: ["WHYANDO", "SG-1-DEVX56"]}}
+                          className="[&.active]:font-bold"
+                        >
+                          {date.resetDate}
+                        </Link>
+                      </TableCell>
+                      <TableCell className="text-right">{prettyDuration(date.durationMinutes * 60 * 1000)}</TableCell>
+                      <TableCell>{date.isOngoing ? <CircleCheckBigIcon /> : <></>}</TableCell>
+                    </TableRow>
+                  ))}
+              </TableBody>
+            </Table>
           </nav>
         </div>
 
